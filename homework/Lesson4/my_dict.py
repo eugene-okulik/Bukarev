1178b6fd04bcbb1731c638732e6165792b672205
Viktor Bukarev <bukar85_1985@mail.ru>
2024-07-23 14:45:13 +0500

    list’: добавьте в конец списка еще один элемен

--- homework/Lesson4/my_dict.py
+++ homework/Lesson4/my_dict.py
@@ -2,3 +2,6 @@ my_dict = {'tuple': (1, 5, 6, 8, 10), 'list': ['test', 1, 2, 'hello', 100],
            'dict': {'test1': 1, 'test2': 6, 'test3': 22, 'test4': 'TEST', 'test5': 77}, 'set': {1, 5, 'Privet', 7, 99}}
 
 print(my_dict['tuple'][-1])
+
+my_dict['list'].append(46)
+print(my_dict)
