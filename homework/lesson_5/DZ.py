b9fa017ef61d73770622f73f8028a8e292e08387
Viktor Bukarev <bukar85_1985@mail.ru>
2024-07-26 22:05:04 +0500

    Сделал через срез и индекс. Возможно не верно

--- homework/lesson_5/DZ.py
+++ homework/lesson_5/DZ.py
@@ -5,29 +5,23 @@ person = ['John', 'Doe', 'New York', '+1372829383739', 'US']
 name, last_name, city, phone, country = person
 print(name, last_name, city, phone, country)
 print(name)
-
+print(symbol)
 z = 'результат операции: 42'
 d = 'результат операции: 514'
 f = 'результат работы программы: 9'
-list_z = z.split()
-list_d = d.split()
-list_f = f.split()
+g = (z.index(':')) + 2
+h = int((z[g:])) + 10
+print(h)
 print(symbol)
-print(list_z)
-print('Индекс = ', list_z.index('42'))
-per_z = int(list_z[2]) + 10
-print(per_z)
+g = (d.index(':')) + 2
+h = int((d[g:])) + 10
+print(h)
 print(symbol)
-
-print('Индекс = ', list_d.index('514'))
-per_d = int(list_d[2]) + 10
-print('Результат сложеия = ', per_d)
+g = (f.index(':')) + 2
+h = int((f[g:])) + 10
+print(h)
 print(symbol)
 
-print('Индекс = ', list_f.index('9'))
-per_f = int(list_f[3]) + 10
-print('Результат сложеия = ', per_f)
-print(symbol)
 
 students = ['Ivanov', 'Petrov', 'Sidorov']
 subjects = ['math', 'biology', 'geography']
