eb6b5d87f88a728a8cffb37524cdf3ea5d25f495
Viktor Bukarev <bukar85_1985@mail.ru>
2024-07-30 14:18:51 +0500

    Исправление ошибок при проверке LINT

--- homework/lesson_6/Lesson6_2.py
+++ homework/lesson_6/Lesson6_2.py
@@ -2,23 +2,19 @@ z = list(range(1, 101))
 print(z)
 c = []
 
-#Числа кратные трем
 for x in z:
     if (x % 3 == 0 and x % 5 == 0):
         x = 'FuzzBuzz'
         c.append(x)
 
     elif x % 3 == 0:
-        #print(x, end=' ')
         o = 'Fuzz'
         c.append(o)
 
     elif x % 5 == 0:
-        #print(x, end=' ')
         p = 'Buzz'
         c.append(p)
     else:
         c.append(x)
 for i in c:
     print(i)
-    
