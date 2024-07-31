076c01afcdd10f39c52b78b0d9cb9a4843eaa774
Viktor Bukarev <bukar85_1985@mail.ru>
2024-07-31 14:18:16 +0500

    урок 7:задание2

---
+++ homework/lesson_7/dict.py
@@ -0,0 +1,9 @@
+words = {'I': 3, 'love': 5, 'Python': 1, '!': 50}
+#print(words)
+z = '=' * 30
+def print_keys(**kwargs):
+    for key, value in kwargs.items():
+        print(key * value)
+        print(z)
+
+print_keys(**words)
