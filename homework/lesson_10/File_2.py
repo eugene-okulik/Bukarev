0e22ad623b7f4c3be46262b865fd5cfb86988c1b
Viktor Bukarev <bukar85_1985@mail.ru>
2024-08-08 22:14:26 +0500

    change File_2

--- homework/lesson_10/File_2.py
+++ homework/lesson_10/File_2.py
@@ -1,12 +1,10 @@
-# Задание №2
-# Создайте универсальный декоратор, который будет управлять тем, сколько раз запускается декорируемая функция
-#
-# Код, использующий этот декоратор может выглядеть, например, так:
-
 def repeat_me(func):
 
     def wrapper(*args, count1):
-        print(f'функция "repeat_me" вызывалась {count1} раз: ', *args * count1, sep='\n')
+        i = 0
+        while i < count1:
+            func(*args)
+            i += 1
     return wrapper
 
 
@@ -15,4 +13,4 @@ def example(text):
     print(text)
 
 
-example('print me', count1=2)
+example('print me', count1=10)
